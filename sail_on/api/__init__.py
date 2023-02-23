5f293d59ded754056db18d9008dcd0d57d89a59f
Irenetema <temateli@submit-a.hpc.engr.oregonstate.edu>
2023-02-23 13:17:12 -0800

    changed FileProviderSVO to FileProviderSS

--- sail_on/api/__init__.py
+++ sail_on/api/__init__.py
@@ -2,7 +2,7 @@
 from .provider import Provider
 from .server import set_provider, init
 from .errors import ServerError, ProtocolError, ApiError, RoundError
-from .file_provider import FileProviderSVO as FileProvider
+from .file_provider import FileProviderSS as FileProvider
 from .constants import ProtocolConstants
 
 __all__ = [
